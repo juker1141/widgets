4e78f64d29a82843e562600654da1172977bc9f8
juker1141 <50107434+juker1141@users.noreply.github.com>
2020-12-23 22:20:29 +0800

    update

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,41 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { cleanup } from '@testing-library/react';
 
 const Search = () => {
-  const [term, setTerm] = useState('dog');
+  const [term, setTerm] = useState('柴犬');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
   useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
+
+  useEffect(() => {
     const search = async () => {
-      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+      const { data } = await axios.get('https://zh.wikipedia.org/w/api.php', {
         params: {
           action: 'query',
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
 
       setResults(data.query.search);
     };
 
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        };
-      }, 1000);
-
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    };
-
-  }, [term]);
+    search();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return (
@@ -43,7 +40,7 @@ const Search = () => {
         <div className="right floated content">
           <a
             className="ui button"
-            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            href={`https://zh.wikipedia.org?curid=${result.pageid}`}
           >
             Go
           </a>
