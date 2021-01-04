4509a1655e243a77cb9efecf15ab5b3a84579717
juker <juker1141@gmail.com>
2021-01-04 17:34:50 +0800

    UPDATE

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Accordion from './component/Accordion';
 import Search from './component/Search';
 import Dropdown from './component/Dropdown';
+import Translate from './component/Translate';
 
 const items = [
   {
@@ -34,20 +35,9 @@ const options = [
 ];
 
 export default () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
-
   return (
     <div>
-      <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-      {showDropdown ?
-        <Dropdown
-          selected={selected}
-          onSelectedChange={setSelected}
-          options={options}
-        /> : null
-      }
-      <h1 style={{ color: selected.value }}>這是一段文字</h1>
-    </div >
+      <Translate />
+    </div>
   );
 }
